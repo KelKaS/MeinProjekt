1f039def9dc70663602052324565727d4740e2b4
Felix Ortner <you@example.com>
2022-02-02 19:45:38 +0100

    fertig

--- main.cpp
+++ main.cpp
@@ -1,17 +1,3 @@
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 #include <QtCore>
 #include <QtGui>
 
@@ -32,15 +18,30 @@ int main(int argc, char *argv[])
    auto *progress = new QProgressBar();
    progress->setMinimum(0);
    progress->setMaximum(100);
-   
-   
-   
-   auto *layout = new QHBoxLayout(mainWindow);
+
+   auto *Ausgabe = new QLabel();
+
+   auto *Eingabe = new QLineEdit();    
+
+
+   auto *layout = new QHBoxLayout();
    layout->addWidget(pb);
    layout->addWidget(slider);
    layout->addWidget(progress);
+
+   
+   auto *layout1 = new QVBoxLayout(mainWindow);
+   layout1->addStretch();
+   layout1->addLayout(layout);
+   layout1->addWidget(Eingabe);
+   layout1->addWidget(Ausgabe);
+   layout1->addStretch();
+
+
    QObject::connect(pb, &QPushButton::clicked, mainWindow, &QWidget::close);
    QObject::connect(slider, &QSlider::valueChanged, progress, &QProgressBar::setValue);
+   QObject::connect(Eingabe, &QLineEdit::textChanged, Ausgabe, &QLabel::setText);
+   
    mainWindow->show();
 
    return app.exec();
